6f44bae1188d45c57e022520ac59b88fd544392e
denismaster <denismaster@outlook.com>
2018-01-24 22:57:36 +0300

    Add DepartmentsAdd component

---
+++ src/Diploms.WebUI/ClientApp/app/departments/components/add/add.component.ts
@@ -0,0 +1,17 @@
+import { Component, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'departments-add',
+    templateUrl: './add.component.html'
+})
+export class DepartmentsAddComponent {
+    form: FormGroup;
+    
+    constructor(private formBuilder: FormBuilder) {
+        this.form = this.formBuilder.group({
+            "name": ["", Validators.required],
+            "institute": ["", Validators.required]
+        });
+    }
+}
