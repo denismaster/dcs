ada264b14d823b9f22ddd108b42da168976012a4
denismaster <denismaster@outlook.com>
2018-01-24 22:55:50 +0300

    Add TextInput component

---
+++ src/Diploms.WebUI/ClientApp/app/shared/forms/input-text/input-text.component.ts
@@ -0,0 +1,13 @@
+import { Component, Inject, Input } from '@angular/core';
+import { AbstractControl, FormControl } from '@angular/forms';
+
+@Component({
+    selector: 'input-text',
+    templateUrl: './input-text.component.html'
+})
+export class TextInputComponent {
+    @Input() title: string = "";
+    @Input() name: string = "textBox";
+    @Input() placeholder: string = "Введите значение";
+    @Input() control: AbstractControl = new FormControl;
+}
