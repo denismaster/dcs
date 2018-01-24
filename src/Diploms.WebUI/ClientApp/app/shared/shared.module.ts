529bb80ba824d68bddc26673c528a37d43942143
denismaster <denismaster@outlook.com>
2018-01-24 22:56:58 +0300

    Add Card and TextInput to the shared module

--- src/Diploms.WebUI/ClientApp/app/shared/shared.module.ts
+++ src/Diploms.WebUI/ClientApp/app/shared/shared.module.ts
@@ -2,6 +2,9 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { TextInputComponent } from './forms/input-text/input-text.component';
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
@@ -9,10 +12,20 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
-    declarations: [ ],
-    exports: [ CommonModule, FormsModule, RouterModule,
-        ReactiveFormsModule ],
-    providers: []
+    declarations: [
+        CardComponent,
+        TextInputComponent,
+    ],
+    exports: [
+        CommonModule,
+        FormsModule,
+        RouterModule,
+        ReactiveFormsModule,
+        CardComponent,
+        TextInputComponent,
+    ],
+    providers: [],
+    schemas: [NO_ERRORS_SCHEMA]
 })
 export class SharedModule {
     static forRoot(): ModuleWithProviders {
