8d6b2a040bf11c5afc3a8d4bf29f1689f9983073
denismaster <denismaster@outlook.com>
2018-01-27 20:18:07 +0300

    Make migration for the changes in a BaseEntity

--- src/Diploms.DataLayer/Migrations/20180127171655_MakeChangeAndRemoveDateNullable.Designer.cs
+++ src/Diploms.DataLayer/Migrations/20180127171655_MakeChangeAndRemoveDateNullable.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Diploms.DataLayer.Migrations
 {
     [DbContext(typeof(DiplomContext))]
-    partial class DiplomContextModelSnapshot : ModelSnapshot
+    [Migration("20180127171655_MakeChangeAndRemoveDateNullable")]
+    partial class MakeChangeAndRemoveDateNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Diploms.DataLayer/Migrations/20180127171655_MakeChangeAndRemoveDateNullable.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Diploms.DataLayer.Migrations
+{
+    public partial class MakeChangeAndRemoveDateNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RemoveDate",
+                table: "Departments",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangeDate",
+                table: "Departments",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RemoveDate",
+                table: "Departments",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ChangeDate",
+                table: "Departments",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
