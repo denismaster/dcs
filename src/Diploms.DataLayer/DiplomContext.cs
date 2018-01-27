6384ee0ce1a2221dc416363128d76bb7f9c28cdd
Denis Obolensky <denis_obolensky@xrm.ru>
2018-01-27 15:22:51 +0300

    Add DiplomContext definition

---
+++ src/Diploms.DataLayer/DiplomContext.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Diploms.Core;
+
+namespace Diploms.DataLayer
+{
+    public class DiplomContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        
+        public DiplomContext(DbContextOptions<DiplomContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //TODO: Use appsettings.json or environment variable, not the string here.
+            optionsBuilder.UseNpgsql(@"Host=localhost;Port=5432;Database=diploms;UserId=postgres;Password=12345678;Pooling=true");
+        }
+    }
+}
