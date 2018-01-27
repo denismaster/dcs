64cf07049c4e07927faeafccf6f50a96a8d4935b
denismaster <denismaster@outlook.com>
2018-01-27 20:12:17 +0300

    Make ChangeDate and RemoveDate fields of BaseEntity nullable

--- src/Diploms.Core/Entities/BaseEntity.cs
+++ src/Diploms.Core/Entities/BaseEntity.cs
@@ -6,7 +6,7 @@ namespace Diploms.Core
     {
         public int Id { get; set; }
         public DateTime CreateDate { get; set; }
-        public DateTime ChangeDate { get; set; }
-        public DateTime RemoveDate { get; set; }
+        public DateTime? ChangeDate { get; set; }
+        public DateTime? RemoveDate { get; set; }
     }
 }
