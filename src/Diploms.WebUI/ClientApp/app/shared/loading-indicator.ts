ad506db3718624dd46a713525a88380a4e249a17
Denis Obolensky <denis_obolensky@xrm.ru>
2018-01-29 13:18:36 +0300

    Add loading indicator component

---
+++ src/Diploms.WebUI/ClientApp/app/shared/loading-indicator.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'loading',
+    template:`<div class="spinner-container">
+    <div class="spinner"></div>
+</div>`
+})
+export class LoadingIndicatorComponent {
+}
