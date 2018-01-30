bbe1962acc83794be0c8433ea2308f742004895f
Denis Obolensky <denis_obolensky@xrm.ru>
2018-01-30 11:43:14 +0300

    Show success message for user

--- src/Diploms.WebUI/ClientApp/app/departments/components/add/add.component.ts
+++ src/Diploms.WebUI/ClientApp/app/departments/components/add/add.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { AlertService } from '../../../shared/alert/services/alert.service';
 
 @Component({
     selector: 'departments-add',
@@ -8,16 +9,21 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DepartmentsAddComponent {
     form: FormGroup;
-    
-    constructor(private formBuilder: FormBuilder,
-    private httpClient: HttpClient) {
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private alertService: AlertService,
+        private httpClient: HttpClient
+    ) {
         this.form = this.formBuilder.group({
             "name": ["", Validators.required],
             "shortName": [""]
         });
     }
 
-    submit(form:any) {
-        this.httpClient.put("/api/departments/add", form).subscribe(e=>{});
+    submit(form: any) {
+        this.httpClient.put("/api/departments/add", form).subscribe(e => { 
+            this.alertService.success("Запись успешно добавлена",true)
+        });
     }
 }
