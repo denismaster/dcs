7ff3e54bcc291da39987823e0be3e0a025316245
Denis Obolensky <denis_obolensky@xrm.ru>
2018-01-30 13:25:13 +0300

    Fill info about department on DepartmentEdit page

--- src/Diploms.WebUI/ClientApp/app/departments/components/edit/edit.component.ts
+++ src/Diploms.WebUI/ClientApp/app/departments/components/edit/edit.component.ts
@@ -1,17 +1,36 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DepartmentsService } from '../../services/departments.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'departments-edit',
     templateUrl: './edit.component.html'
 })
-export class DepartmentsEditComponent {
+export class DepartmentsEditComponent implements OnInit {
     form: FormGroup;
-    
-    constructor(private formBuilder: FormBuilder) {
+    id: number;
+
+    constructor(
+        private router: Router,
+        private activatedRoute: ActivatedRoute,
+        private formBuilder: FormBuilder,
+        private departmentsService: DepartmentsService
+    ) {
+        this.id = activatedRoute.snapshot.params['id'];
+        if (!this.id) {
+            router.navigateByUrl("['/404']");
+        }
         this.form = this.formBuilder.group({
+            "id": [this.id, Validators.required],
             "name": ["", Validators.required],
-            "institute": ["", Validators.required]
+            "shortName": [""]
         });
+    }
+
+    ngOnInit() {
+        this.departmentsService.getDepartment(this.id).subscribe(result => {
+            this.form.setValue(result);
+        })
     }
 }
