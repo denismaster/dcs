602e6e4aaccd8878a3fb7f21a0f2ec8d65854eeb
Denis Obolensky <denis_obolensky@xrm.ru>
2018-02-03 11:20:27 +0300

    Add OperationResult

---
+++ src/Diploms.Dto/OperationResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Diploms.Dto
+{
+    public class OperationResult
+    {
+        public List<string> Errors { get; set; }
+        
+        public bool HasError => Errors.Count!=0;
+    }
+}
