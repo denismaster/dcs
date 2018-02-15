34d54572174835aa727daa612fdd2bf186afaaa0
Denis Obolensky <denis_obolensky@xrm.ru>
2018-02-15 17:23:24 +0300

    Modify SignIn component

--- src/Diploms.WebUI/ClientApp/app/components/sign-in/sign-in.component.ts
+++ src/Diploms.WebUI/ClientApp/app/components/sign-in/sign-in.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { AlertService } from '../../shared/alert/services/alert.service';
 
 @Component({
     selector: 'sign-in',
     templateUrl: './sign-in.component.html',
 })
 export class SignInComponent {
+    constructor(private alertService: AlertService) { }
+
+    model:any={};
+    login(){
+        this.alertService.error("Неверный логин или пароль");
+    }
 }
