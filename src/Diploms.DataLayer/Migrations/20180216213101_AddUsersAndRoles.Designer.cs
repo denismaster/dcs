35f6db9acd1e9a2829ddfed7d0e9b79629109c2a
denismaster <denismaster@outlook.com>
2018-02-17 00:59:14 +0300

    Add migration for Role and Auth

--- src/Diploms.DataLayer/Migrations/20180216213101_AddUsersAndRoles.Designer.cs
+++ src/Diploms.DataLayer/Migrations/20180216213101_AddUsersAndRoles.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Diploms.DataLayer.Migrations
 {
     [DbContext(typeof(DiplomContext))]
-    partial class DiplomContextModelSnapshot : ModelSnapshot
+    [Migration("20180216213101_AddUsersAndRoles")]
+    partial class AddUsersAndRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
