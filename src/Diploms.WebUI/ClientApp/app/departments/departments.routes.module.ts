e17308be99539204c3d2ffa558c260f1432a14ff
denismaster <denismaster@outlook.com>
2018-02-19 13:01:48 +0300

    Add AuthGuard

--- src/Diploms.WebUI/ClientApp/app/departments/departments.routes.module.ts
+++ src/Diploms.WebUI/ClientApp/app/departments/departments.routes.module.ts
@@ -3,19 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { DepartmentsListComponent } from './components/list/list.component';
 import { DepartmentsAddComponent } from './components/add/add.component';
 import { DepartmentsEditComponent } from './components/edit/edit.component';
+import { AuthGuard } from '../auth/guards/auth.guard';
 
 const routes: Routes = [
     {
         path: 'departments',
         component: DepartmentsListComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'departments/new',
         component: DepartmentsAddComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'departments/:id',
         component: DepartmentsEditComponent,
+        canActivate: [AuthGuard]
     },
 ];
 
