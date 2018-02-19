9b04cc7b39a143e17cfc1d572c23193818af6711
denismaster <denismaster@outlook.com>
2018-02-19 13:05:06 +0300

    Modify navigation menu to show info about user login status

--- src/Diploms.WebUI/ClientApp/app/components/navmenu/navmenu.component.ts
+++ src/Diploms.WebUI/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService, getUserAuthInfo } from '../../auth/services/auth.service';
+import { AuthInfoService } from '../../auth/services/auth-info.service';
 
 @Component({
     selector: 'nav-menu',
     templateUrl: './navmenu.component.html'
 })
 export class NavMenuComponent {
+
+    username: string | undefined;
+    constructor(private authInfo: AuthInfoService, private authService: AuthService) { }
+
+    ngOnInit() {
+        this.authInfo.getUserAuthInfo().subscribe(info => {
+            if (!info) {
+                this.username = undefined;
+                return;
+            }
+
+            this.username = "logged in"
+        })
+    }
+
+    logout(){
+        this.authService.logout();
+    }
 }
