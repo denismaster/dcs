b04edae16d6ff5fe7c256467513282ee01c94c4a
denismaster <denismaster@outlook.com>
2018-02-19 13:02:47 +0300

    Modify Login Component to call API and authenticate

--- src/Diploms.WebUI/ClientApp/app/components/sign-in/sign-in.component.ts
+++ src/Diploms.WebUI/ClientApp/app/components/sign-in/sign-in.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { AlertService } from '../../shared/alert/services/alert.service';
+import { AuthRequest, AuthService } from '../../auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'sign-in',
     templateUrl: './sign-in.component.html',
 })
 export class SignInComponent {
-    constructor(private alertService: AlertService) { }
+    constructor(private authService:AuthService, private router: Router ,private alertService: AlertService) { }
 
-    model:any={};
+    model= {
+        login: "", password:"", remember:true
+     };
     login(){
-        this.alertService.error("Неверный логин или пароль");
+        const authRequest: AuthRequest = {
+            userName: this.model.login,
+            password: this.model.password,
+            rememberMe: this.model.remember
+        };
+        this.authService.login(authRequest).subscribe(_=>{
+            this.router.navigate(['/']);
+        }, error=>this.alertService.error("Неверный логин или пароль"));
     }
 }
