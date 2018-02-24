ae0de0be680a3c0d372c034745b1f18dbbf00855
Denis Obolensky <denis_obolensky@xrm.ru>
2018-02-24 15:17:01 +0300

    Add InstituteService

---
+++ src/Diploms.Services/Institutes/InstitutesService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using Diploms.Core;
+using Diploms.Dto;
+using Diploms.Dto.Institutes;
+
+namespace Diploms.Services.Institutes
+{
+    public class InstitutesService : CrudService<Institute, InstituteEditDto, InstituteEditDto, InstituteEditDto>
+    {
+        public InstitutesService(IRepository<Institute> repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+    }
+}
