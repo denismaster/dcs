7e45511a252c25c21ae81addc8c496a824e4007b
Denis Obolensky <denis_obolensky@xrm.ru>
2018-02-24 15:16:32 +0300

    Add Institute and Speciality EditDto's

---
+++ src/Diploms.Dto/Institutes/InstituteEditDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Diploms.Dto.Institutes
+{
+    public class InstituteEditDto
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
