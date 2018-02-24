0257831945ccf76f2cc38fe46170c021ea39dcaf
Denis Obolensky <denis_obolensky@xrm.ru>
2018-02-24 15:20:32 +0300

    Add new methods to StartupExtensions to use Institutes and Specialitites

--- src/Diploms.WebUI/Configuration/StartupExtensions.cs
+++ src/Diploms.WebUI/Configuration/StartupExtensions.cs
@@ -1,12 +1,28 @@
 using Diploms.Core;
 using Diploms.DataLayer;
 using Diploms.Services.Departments;
+using Diploms.Services.Specialities;
+using Diploms.Services.Institutes;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Diploms.WebUI.Configuration
 {
     public static class StartupExtensions
     {
+        public static IServiceCollection AddInstitutes(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository<Institute>, RepositoryBase<Institute>>();
+            services.AddScoped<InstitutesService>();
+            return services;
+        }
+
+        public static IServiceCollection AddSpecialities(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository<Speciality>, RepositoryBase<Speciality>>();
+            services.AddScoped<SpecialitiesService>();
+            return services;
+        }
+
         public static IServiceCollection AddDepartments(this IServiceCollection services)
         {
             services.AddScoped<IRepository<Department>, RepositoryBase<Department>>();
