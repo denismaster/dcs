2503cb6fd6d9c16f727c7812d412483d931609a6
Denis Obolensky <denis_obolensky@xrm.ru>
2018-02-24 15:18:39 +0300

    Modify DepartmentService to use new CrudService

--- src/Diploms.Services/Departments/DepartmentsService.cs
+++ src/Diploms.Services/Departments/DepartmentsService.cs
@@ -7,55 +7,10 @@ using Diploms.Dto.Departments;
 
 namespace Diploms.Services.Departments
 {
-    public class DepartmentsService 
+    public class DepartmentsService : CrudService<Department, DepartmentEditDto, DepartmentEditDto, DepartmentEditDto>
     {
-        private readonly IRepository<Department> _repository;
-        private readonly IMapper _mapper;
-
-        public DepartmentsService(IRepository<Department> repository, IMapper mapper)
+        public DepartmentsService(IRepository<Department> repository, IMapper mapper) : base(repository, mapper)
         {
-            _repository = repository ?? throw new System.ArgumentNullException(nameof(repository));
-            _mapper = mapper ?? throw new System.ArgumentNullException(nameof(mapper));
-        }
-
-        public async Task<OperationResult> Add(DepartmentEditDto model)
-        {
-            var result = new OperationResult();
-
-            try
-            {
-                var department  = _mapper.Map<Department>(model);
-                department.CreateDate = DateTime.UtcNow;
-                _repository.Add(department);
-
-                await _repository.SaveChanges();
-            }
-            catch(Exception e)
-            {
-                result.Errors.Add(e.Message);
-            }
-            
-            return result;
-        }
-
-        public async Task<OperationResult> Edit(DepartmentEditDto model)
-        {
-            var result = new OperationResult();
-
-            try
-            {
-                var department  = _mapper.Map<Department>(model);
-                department.ChangeDate = DateTime.UtcNow;
-                _repository.Update(department);
-
-                await _repository.SaveChanges();
-            }
-            catch(Exception e)
-            {
-                result.Errors.Add(e.Message);
-            }
-            
-            return result;
         }
     }
 }
