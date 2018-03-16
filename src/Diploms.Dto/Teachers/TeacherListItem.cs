a95550324e591152c14ab638b5783a132928bc50
denismaster <denismaster@outlook.com>
2018-03-16 23:58:58 +0300

    Add dto for Teacher's list item

---
+++ src/Diploms.Dto/Teachers/TeacherListItem.cs
@@ -0,0 +1,12 @@
+namespace DiplomContentSystem.Dto
+{
+    public class TeacherListItem
+    {
+        public int Id {get;set;}
+        public string FIO {get;set;}
+        public string Position {get;set;}
+        public string Department {get;set;}
+        public int WorkCount {get;set;}
+        public int MaxWorkCount {get;set;}
+    }
+}
