92dc911ceb4da1258e33a1c7efda62ae78f7816e
denismaster <denismaster@outlook.com>
2018-03-16 23:58:36 +0300

    Add dto for DiplomWorks list

---
+++ src/Diploms.Dto/DiplomWorks/DiplomWorkListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiplomContentSystem
+{
+    public class DiplomWorkListItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public int? TeacherId {get;set;}
+        public string Teacher { get; set; }
+        public int? StudentId { get; set; }
+        public string Student { get; set; }
+    }
+}
