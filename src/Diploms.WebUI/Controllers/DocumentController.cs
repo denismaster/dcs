c87887b1861d139557569e05e2f83cd40ee4a5d7
Denis Obolensky <denis_obolensky@xrm.ru>
2018-05-05 12:06:11 +0300

    Add request service and document controller

---
+++ src/Diploms.WebUI/Controllers/DocumentController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Diploms.Dto;
+using Diploms.Services.Departments;
+using Diploms.Requests;
+using System.Threading.Tasks;
+using Microsoft.Net.Http.Headers;
+
+namespace Diploms.Controllers
+{
+    [Route("api/documents")]
+    [Authorize]
+    public class DocumentsController : Controller
+    {
+        private readonly RequestService _service;
+        public DocumentsController(RequestService service)
+        {
+            if (service == null) throw new ArgumentNullException(nameof(service));
+            _service = service;
+        }
+        [HttpPost("latex/preview")]
+        public async Task<IActionResult> GeneratePreviewLatex([FromBody] string latex)
+        {
+            var stream = await _service.SendRequest(latex, false);
+            return new FileStreamResult(stream,"application/pdf");
+        }
+    }
+}
---
+++ src/Diploms.WebUI/Requests/RequestService.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Diploms.Requests
+{
+    public class RequestService
+    {
+        public async Task<Stream> SendRequest(object data, bool isDocx = true)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(data,
+                    new JsonSerializerSettings()
+                    {
+                        ContractResolver = new CamelCasePropertyNamesContractResolver()
+                    }), System.Text.Encoding.UTF8, "application/json");
+                    client.BaseAddress = new Uri("http://localhost:1337");
+
+                    var address = isDocx ? "api/docx" : "api/latex";
+
+                    var response = await client.PostAsync(address, content);
+                    response.EnsureSuccessStatusCode(); // Throw in not success
+
+                    return await response.Content.ReadAsStreamAsync();
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Request exception: {e.Message}");
+                    throw (e);
+                }
+            }
+        }
+    }
+}
